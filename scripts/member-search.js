ce8bd52cbeb78ae2a52702e0ef0d65a64a8f916c
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2019-12-01 02:05:54 +0530

    add volunteer search feature

---
+++ scripts/member-search.js
@@ -0,0 +1,33 @@
+const validIds = ["MH-SU-137"];
+const idCardImg = document.getElementById('idCardImg');
+const memberIdErrorField = document.getElementById('memberIdErrorField');
+
+function onMemberSearch(e) {
+    e.preventDefault();
+
+    const enteredValue = document.getElementById('memberIdField').value.toUpperCase();
+
+    let image = new Image();
+    const imgSrc = `../media/id-cards/${enteredValue}.jpeg`;
+
+    image.onload = () => {
+        console.log('loaded')
+        idCardImg.setAttribute('src', imgSrc);
+        idCardDownloadLink.setAttribute('href', imgSrc);
+        memberIdErrorField.style.display = 'none';
+    }
+
+    image.onerror = () => {
+        console.log('error')
+        idCardImg.setAttribute('src', '');
+        idCardDownloadLink.setAttribute('href', '');
+        memberIdErrorField.style.display = 'block';
+    }
+
+    image.src = imgSrc
+
+    if (image.complete) {
+        console.log('complete')
+        image.onload.call(image)
+    }
+}
