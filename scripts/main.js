a84e21cbad555a4c3114f85304637a4fbd8dd292
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2019-11-30 16:56:39 +0530

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,70 @@
 $(document).ready(function () {
+    /* NAVBAR LOGIC */
+
+    const showNavBar = () => {
+        $(".navbar").css({
+            visibility: "visible",
+            top: "0",
+            "transition-delay": "0s"
+        });
+    };
+
+    const hideNavBar = () => {
+        $(".navbar").css({
+            visibility: "hidden",
+            top: "-200px",
+            transition: "visibility 0s ease 0.7s, top 0.7s ease"
+        });
+    };
+
     let lastScrollTop = 0;
-    $(window).on('scroll', function () {
-        if ($(this).scrollTop() > 0) { // if scrolled
-            $('.navbar').css({ 
-                'border-bottom': '1px solid #f9f9f9'
-            });
+
+    $(window).on("scroll", () => {
+        if ($(this).scrollTop() < 10) {
+            showNavBar();
+            return;
         }
-        
-        if ($(this).scrollTop() > lastScrollTop) { // if scrolled downwards
-            $('.navbar').css({ 
-                'padding-top': '0px',
-                'padding-bottom': '0px',
-            });
-        } else { // if scrolled upwards
-            $('.navbar').css({ 
-                'padding-top': '5px',
-                'padding-bottom': '5px'
-            });
+
+        if (Math.abs($(this).scrollTop() - lastScrollTop) < 10) return;
+
+        if ($(this).scrollTop() < lastScrollTop) {
+            // if scrolled upwards..
+            showNavBar();
+            console.log('show')
         }
+        else {
+            // if scrolled downwards..
+            hideNavBar();
+            console.log('hode')
+        }
+
+        // update last scroll position
         lastScrollTop = $(this).scrollTop();
     });
 
-}); 
+    $(window).on("resize", () => {
+        if ($(window).width() < 400) {
+            $("#navbar-title").text("F.H.R.F.");
+            $("#navbar-title").css({
+                "font-size": "1.5em"
+            });
+        } else if ($(window).width() < 450) {
+            $("#navbar-title").text("FIRST HUMAN RIGHTS FOUNDATION");
+            $("#navbar-title").css({
+                "font-size": "0.8em"
+            });
+        } else if ($(window).width() < 525) {
+            $("#navbar-title").text("FIRST HUMAN RIGHTS FOUNDATION");
+            $("#navbar-title").css({
+                "font-size": "1em"
+            });
+        } else {
+            $("#navbar-title").text("FIRST HUMAN RIGHTS FOUNDATION");
+            $("#navbar-title").css({
+                "font-size": "1.25em"
+            });
+        }
+    });
+
+    /* ENDOF NAVBAR LOGIC */
+});
