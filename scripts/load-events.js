ff711246b64411700bfb585b4cf43debd5eca8a8
Pratik Gadhiya <pratik.gadhiya@hotmail.com>
2019-12-01 23:59:08 +0530

    apply uniform font style

--- scripts/load-events.js
+++ scripts/load-events.js
@@ -12,10 +12,10 @@ $(document).ready(function () {
                 let eventDom = new DOMParser().parseFromString(
                     `<div class="row mt-5">
                     <div class="col-12">
-                        <h3>${event.name}</h3>
+                        <h3 class="font-heading">${event.name}</h3>
                     </div>
                     <div class="col-12">
-                        <p>${event.description}</p>
+                        <p class="font-content">${event.description}</p>
                     </div>
 
                     <div class="col-12 imgContainer">
