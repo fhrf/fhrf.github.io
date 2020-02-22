a392344f245e704e7cf4524921029582275beb75
Pratik Pravinbhai Gadhiya <pratik.gadhiya@infosys.com>
2020-02-22 12:50:43 +0530

    format files with prettier

--- scripts/load-events.js
+++ scripts/load-events.js
@@ -1,16 +1,15 @@
-$(document).ready(function () {
-    let eventsData = [];
-    const eventsContainer = document.getElementById('events-container');
+$(document).ready(function() {
+  let eventsData = [];
+  const eventsContainer = document.getElementById("events-container");
 
-    fetch('/events-data.json')
-        .then(response => response.json())
-        .then(data => {
-            eventsData = data;
+  fetch("/events-data.json")
+    .then(response => response.json())
+    .then(data => {
+      eventsData = data;
 
-            eventsData.forEach(event => {
-
-                let eventDom = new DOMParser().parseFromString(
-                    `<div class="row mt-5">
+      eventsData.forEach(event => {
+        let eventDom = new DOMParser().parseFromString(
+          `<div class="row mt-5">
                     <div class="col-12">
                         <h3 class="font-heading">${event.name}</h3>
                     </div>
@@ -19,16 +18,17 @@ $(document).ready(function () {
                     </div>
 
                     <div class="col-12 imgContainer">
-                        ${
-                    event.files.map(file =>
-                        `<img src="media/activities/${event.folderName}/${file}">`
-                    )}
+                        ${event.files.map(
+                          file =>
+                            `<img src="media/activities/${event.folderName}/${file}">`
+                        )}
                     </div>
-                </div>`, 'text/html').body.firstChild;
+                </div>`,
+          "text/html"
+        ).body.firstChild;
 
-                eventsContainer.appendChild(eventDom)
-
-            });
-        })
-        .catch();
-})
+        eventsContainer.appendChild(eventDom);
+      });
+    })
+    .catch();
+});
--- scripts/member-search.js
+++ scripts/member-search.js
@@ -1,33 +1,35 @@
 const validIds = ["MH-SU-137"];
-const idCardImg = document.getElementById('idCardImg');
-const memberIdErrorField = document.getElementById('memberIdErrorField');
+const idCardImg = document.getElementById("idCardImg");
+const memberIdErrorField = document.getElementById("memberIdErrorField");
 
 function onMemberSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const enteredValue = document.getElementById('memberIdField').value.toUpperCase();
+  const enteredValue = document
+    .getElementById("memberIdField")
+    .value.toUpperCase();
 
-    let image = new Image();
-    const imgSrc = `../media/id-cards/${enteredValue}.jpeg`;
+  let image = new Image();
+  const imgSrc = `../media/id-cards/${enteredValue}.jpeg`;
 
-    image.onload = () => {
-        console.log('loaded')
-        idCardImg.setAttribute('src', imgSrc);
-        idCardDownloadLink.setAttribute('href', imgSrc);
-        memberIdErrorField.style.display = 'none';
-    }
+  image.onload = () => {
+    console.log("loaded");
+    idCardImg.setAttribute("src", imgSrc);
+    idCardDownloadLink.setAttribute("href", imgSrc);
+    memberIdErrorField.style.display = "none";
+  };
 
-    image.onerror = () => {
-        console.log('error')
-        idCardImg.setAttribute('src', '');
-        idCardDownloadLink.setAttribute('href', '');
-        memberIdErrorField.style.display = 'block';
-    }
+  image.onerror = () => {
+    console.log("error");
+    idCardImg.setAttribute("src", "");
+    idCardDownloadLink.setAttribute("href", "");
+    memberIdErrorField.style.display = "block";
+  };
 
-    image.src = imgSrc
+  image.src = imgSrc;
 
-    if (image.complete) {
-        console.log('complete')
-        image.onload.call(image)
-    }
+  if (image.complete) {
+    console.log("complete");
+    image.onload.call(image);
+  }
 }
