08c2b0512feb7c437743cefcdab64b9a10233a9e
Pratik Pravinbhai Gadhiya <pratik.gadhiya@infosys.com>
2020-04-13 19:29:56 +0530

    add members

--- scripts/member-search.js
+++ scripts/member-search.js
@@ -1,4 +1,49 @@
-const validIds = ["MH-SU-137"];
+const validIds = [
+  {
+    id: "DL-SU-192",
+    file: "DL-SU-192.jpg",
+  },
+  {
+    id: "MH-DU-142",
+    file: "MH-DU-142.jpg",
+  },
+  {
+    id: "DH-DU-193",
+    file: "DH-DU-193.jpg",
+  },
+  {
+    id: "DH-DU-194",
+    file: "DH-DU-194.jpg",
+  },
+  {
+    id: "MH-SU-137",
+    file: "MH-SU-137.jpg",
+  },
+  {
+    id: "MH-SU-189",
+    file: "MH-SU-189.jpg",
+  },
+  {
+    id: "MH-SU-190",
+    file: "MH-SU-190.jpg",
+  },
+  {
+    id: "MH-SU-191",
+    file: "MH-SU-191.jpg",
+  },
+  {
+    id: "MH-SU-193",
+    file: "MH-SU-193.jpg",
+  },
+  {
+    id: "TS-NU-146",
+    file: "TS-NU-146.jpg",
+  },
+  {
+    id: "TS-SU-147",
+    file: "TS-SU-147.jpg",
+  },
+];
 const idCardImg = document.getElementById("idCardImg");
 const memberIdErrorField = document.getElementById("memberIdErrorField");
 
@@ -9,27 +54,30 @@ function onMemberSearch(e) {
     .getElementById("memberIdField")
     .value.toUpperCase();
 
-  let image = new Image();
-  const imgSrc = `../media/id-cards/${enteredValue}.jpeg`;
+  const validId = validIds.find((validid) => validid.id === enteredValue);
 
-  image.onload = () => {
-    console.log("loaded");
-    idCardImg.setAttribute("src", imgSrc);
-    idCardDownloadLink.setAttribute("href", imgSrc);
-    memberIdErrorField.style.display = "none";
-  };
+  if (validId) {
+    let image = new Image();
+    const imgSrc = `../media/id-cards/${validId.file}`;
 
-  image.onerror = () => {
-    console.log("error");
-    idCardImg.setAttribute("src", "");
-    idCardDownloadLink.setAttribute("href", "");
-    memberIdErrorField.style.display = "block";
-  };
+    image.onload = () => {
+      idCardImg.setAttribute("src", imgSrc);
+      idCardDownloadLink.setAttribute("href", imgSrc);
+      memberIdErrorField.style.display = "none";
+    };
 
-  image.src = imgSrc;
+    image.onerror = () => {
+      idCardImg.setAttribute("src", "");
+      idCardDownloadLink.setAttribute("href", "");
+      memberIdErrorField.style.display = "block";
+    };
 
-  if (image.complete) {
-    console.log("complete");
-    image.onload.call(image);
+    image.src = imgSrc;
+
+    if (image.complete) {
+      image.onload.call(image);
+    }
+  } else {
+    alert("Invalid id! Please retry!");
   }
 }
